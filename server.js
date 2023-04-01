b072e5cd886326a3ab8b00f64c67a9e1f1615f8a
Maylon-tech <minatokiroisen77@gmail.com>
2023-04-01 23:31:24 +0900

    starting the backend structure

---
+++ server.js
@@ -0,0 +1,19 @@
+// Imports
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const session = require('express-session')
+
+const app = express()
+const PORT = process.env.PORT || 4000
+
+app.get('/', (req, res) => {
+    res.send('Hello maylon react developer.')
+})
+
+
+
+app.listen(PORT, () => {
+    console.log('Server is running on port PORT.... the usual')
+})
